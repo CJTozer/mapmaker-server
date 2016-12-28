e9ea3470dbcfb0d414c29b9b940c797d6ca13fff
CJTozer <christopherjtozer@gmail.com>
2016-12-28 22:13:02 +0000

    Initial skeleton for mapmaker-server

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+/* jshint esversion: 6 */
+"use strict";
+
+const chalk = require("chalk");
+const http = require('http');
+
+// Port - @@@ Don't override if provided by ENV.
+const PORT=5000;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+  switch (request.method) {
+    case "POST":
+      // @@@ Do the work here...
+      request.on('data', (chunk) => {
+        console.log(`Received data: ${chunk}`);
+        response.end("Done + chunk");
+      });
+      break;
+    default:
+      // Not expected - error.
+      console.log(chalk.bold.red(`Unexpected request: ${request.method} for ${request.url}`));
+      response.statusCode = 404;
+      response.end("mapmaker-server only accepts POST requests");
+      break;
+  }
+}
+
+// Create and start a server.
+http.createServer(handleRequest).listen(PORT, function(){
+  console.log(chalk.bold.green(`mapmaker-server listening on port ${PORT}`));
+});
