3c2f24fd08216c4ba74b86ffdbc5b4c9bc7393d1
CJTozer <christopherjtozer@gmail.com>
2016-12-29 18:03:13 +0000

    map-maker server working!

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ const PORT=5000;
 function handleRequest(request, response){
   switch (request.method) {
     case "POST":
-      // @@@ Do the work here...
       request.on('data', (chunk) => {
         console.log(`Received data: ${chunk}`);
-        response.end("Done + chunk");
+        var mm = new MapMaker()
+          .spec(JSON.parse(chunk))
+          .onError((err) => {
+            console.log(chalk.bold.red(err));
+            response.statusCode = 404;
+            response.end(err);
+          })
+          .onSuccess((data) => {
+            response.end(data);
+          });
+        mm.build_map();
       });
       break;
     default:
@@ -30,6 +39,6 @@ function handleRequest(request, response){
 
 // Create and start a server.
 http.createServer(handleRequest).listen(PORT, function(){
+  process.env.debug = true; // @@@ For now...
   console.log(chalk.bold.green(`mapmaker-server listening on port ${PORT}`));
-  MapMaker.test();
 });
