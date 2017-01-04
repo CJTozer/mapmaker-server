eabbfe7961ea307c84728a3ebd75f8ec1dcffa06
CJTozer <christopherjtozer@gmail.com>
2017-01-04 20:06:54 +0000

    package.json commit and lint fixes

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
 #!/usr/bin/env node
 /* jshint esversion: 6 */
-"use strict";
+'use strict';
 
-const chalk = require("chalk");
-const http = require('http');
-const MapMaker = require('map-maker');
-
-// Port - @@@ Don't override if provided by ENV.
-const PORT=5000;
+const
+  chalk = require( 'chalk' ),
+  http = require( 'http' ),
+  MapMaker = require( 'map-maker' ),
+  // Port - @@@ Don't override if provided by ENV.
+  PORT = 5000;
 
 //We need a function which handles requests and send response
-function handleRequest(request, response){
-  switch (request.method) {
-    case "POST":
-      request.on('data', (chunk) => {
-        console.log(`Received data: ${chunk}`);
-        new MapMaker()
-          .spec(JSON.parse(chunk))
-          .onError((err) => {
-            console.log(chalk.bold.red(err));
+function handleRequest( request, response ) {
+  switch ( request.method ) {
+  case 'POST':
+    request.on( 'data', ( chunk ) => {
+      console.log( `Received data: ${chunk}` );
+      new MapMaker()
+          .spec( JSON.parse( chunk ) )
+          .onError( ( err ) => {
+            console.log( chalk.bold.red( err ) );
             response.statusCode = 404;
-            response.end(err);
-          })
-          .onSuccess((data) => {
-            response.end(data);
-          })
+            response.end( err );
+          } )
+          .onSuccess( ( data ) => {
+            response.end( data );
+          } )
           .build_map();
-      });
-      break;
-    default:
+    } );
+    break;
+  default:
       // Not expected - error.
-      console.log(chalk.bold.red(`Unexpected request: ${request.method} for ${request.url}`));
-      response.statusCode = 404;
-      response.end("mapmaker-server only accepts POST requests");
-      break;
+    console.log( chalk.bold.red( `Unexpected request: ${request.method} for ${request.url}` ) );
+    response.statusCode = 404;
+    response.end( 'mapmaker-server only accepts POST requests' );
+    break;
   }
 }
 
 // Create and start a server.
-http.createServer(handleRequest).listen(PORT, function(){
-  process.env.debug = true; // @@@ For now...
-  console.log(chalk.bold.green(`mapmaker-server listening on port ${PORT}`));
-});
+http.createServer( handleRequest ).listen( PORT, function() {
+  // @@@ For now always use debug logging...
+  process.env.debug = true;
+  console.log( chalk.bold.green( `mapmaker-server listening on port ${PORT}` ) );
+} );
